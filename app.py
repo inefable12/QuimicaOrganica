919a81fb404aa79037fe662c4d81e357ae763c36
Jesus_Alvarado-Huayhuaz <inefable12@gmail.com>
2024-09-20 14:13:12 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ import pubchempy as pcp
 st.title("Recursos para Química Orgánica")
 st.text("Autor: Jesus Alvarado")
 
+st.title("Nombre común a IUPAC")
+st.text("")
+
 #moleculaA = "https://raw.githubusercontent.com/inefable12/QuimicaOrganica/refs/heads/main/moleculas/A_gluc.pdb"
 entrada = st.text_input("Escribe el nombre de una molécula en inglés:", "glucose")
 s= pcp.get_compounds(entrada,'name')
